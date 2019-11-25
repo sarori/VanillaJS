1a166cb19677fda3ef3488a5b6969580cdfece35
sarori <salo3678@gmail.com>
2019-11-25 11:58:23 +0100

    .

---
+++ clock.js
@@ -0,0 +1,21 @@
+const clockContainer = document.querySelector(".js-clock");
+const clockTitle = clockContainer.querySelector("h1");
+
+function getTime(){
+    const date = new Date();
+    const minites = date.getMinutes();
+    const hours = date.getHours();
+    const seconds = date.getSeconds();
+    clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${minites < 10 ? `0${minites}` : minites}:${
+        seconds < 10 ? `0${seconds}` : seconds
+    }`;
+
+}
+
+function init() {
+    getTime();
+    setInterval(getTime, 1000);
+
+}
+
+init();
